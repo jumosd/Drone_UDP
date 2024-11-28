a0d964ce203d8082674373e2d91656501c8680b7
jumosd <jumosd@icloud.com>
2024-11-28 10:09:54 +0900

    테스트

---
+++ Assets/02.Script/Compass.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public RectTransform compassUI; // 나침반 UI (Image)
+ 
+
+    void Update()
+    {
+        // 드론의 현재 방향 벡터
+        Vector3 forward = transform.forward;
+
+        // 헤딩 각도 계산 (X, Z 평면 기준)
+        float headingAngle = Mathf.Atan2(forward.x, forward.z) * Mathf.Rad2Deg;
+
+        // 각도를 0~360도로 변환
+        if (headingAngle < 0) headingAngle += 360;
+
+        Debug.Log($"Drone Heading Angle: {headingAngle}°");
+    }
+}
